61061b376b139a7fa75b420d9090a10db69058ff
Alexandr Fokin <shurik_f@rambler.ru>
2024-10-31 23:25:50 +0300

    31.10.2024

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -15,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request,f"{user.username},'Вы успешно вошли!'")
                 return HttpResponseRedirect(reverse("main:index"))
 
     else:
@@ -42,14 +44,25 @@ def registration(request):
     }
     return render(request, "users/registration.html", context=context)
 
-
+@login_required
 def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Ваш профиль обновлен!")
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context: dict[str, str] = {
         "title": "Дубок - Кабинет",
+        "form": form,
     }
     return render(request, "users/profile.html", context=context)
 
 
 def logout(request):
+    messages.success(request, f'{request.user.username}"Вы вышли из аккаунта!"')
     auth.logout(request)
     return redirect(reverse("main:index"))
